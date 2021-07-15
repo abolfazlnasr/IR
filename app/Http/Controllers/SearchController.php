a70743ce24e03d0ba64320af75438f2b29794ec9
abolfazlnasr <49614051+abolfazlnasr@users.noreply.github.com>
2021-07-15 08:47:01 +0430

    refactor last commit

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -11,7 +11,6 @@ class SearchController extends Controller
 
     public function search(Request $request)
     {
-        dd(6321);
         $query = $request->get('query');
 
         $client = ClientBuilder::create()->build();
@@ -37,31 +36,31 @@ class SearchController extends Controller
         return view('search', ['results' => $results]);
     }
 
-//    public function insertIntoElastic(Request $request): void
-//    {
-//        $client = ClientBuilder::create()->build();
-//
-//        $params = [
-//            'index' => 'jobs',
-//        ];
-//
-//        $pages = Page::all();
-//
-//        foreach ($pages as $page) {
-//            $params['id'] = $page->id;
-//
-//            $params['body'] = [
-//                'title' => $page->title,
-//                'company' => $page->company,
-//                'level' => $page->level,
-//                'contract_type' => $page->contract_type,
-//                'link' => $page->link,
-//                'description' => $page->description
-//            ];
-//
-//            $client->index($params);
-//        }
-//
-//        echo "insert completed.<br>";
-//    }
+    public function insertIntoElastic(Request $request): void
+    {
+        $client = ClientBuilder::create()->build();
+
+        $params = [
+            'index' => 'jobs',
+        ];
+
+        $pages = Page::all();
+
+        foreach ($pages as $page) {
+            $params['id'] = $page->id;
+
+            $params['body'] = [
+                'title' => $page->title,
+                'company' => $page->company,
+                'level' => $page->level,
+                'contract_type' => $page->contract_type,
+                'link' => $page->link,
+                'description' => $page->description
+            ];
+
+            $client->index($params);
+        }
+
+        echo "insert completed.<br>";
+    }
 }
